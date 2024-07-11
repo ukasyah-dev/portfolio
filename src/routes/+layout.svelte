33d846bd2d06bec8ecc7af35293c78bc14efc4ba
Ukasyah <hi.ukasyah@gmail.com>
2024-07-11 07:14:05 +0700

    update home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+		rel="stylesheet"
+	/>
+	<title>Ukasyah • Full-Stack Developer</title>
+	<meta
+		name="description"
+		content="Specializing in creating user-friendly and high-performance web applications."
+	/>
+</svelte:head>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-full px-5 py-6 flex flex-col items-center justify-center text-center">
+	<h1 class="text-2xl md:text-4xl font-bold leading-normal md:leading-snug">
+		Hi. I'm Ukasyah, <br /> Full-Stack Developer.
+	</h1>
+	<p class="text-base md:text-lg leading-relaxed md:leading mt-2.5 md:mt-3.5 max-w-md">
+		Specializing in creating user-friendly and high-performance web applications.
+	</p>
+</div>
